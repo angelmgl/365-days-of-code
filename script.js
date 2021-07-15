3681c73b9a64d79fc90d50269e3e6605f9b44d62
Angel Mgl <angelemegeele@gmail.com>
2021-07-14 20:19:14 -0400

    Add landing page

---
+++ script.js
@@ -0,0 +1,15 @@
+const list = document.querySelector("#list");
+
+fetch("data.json")
+    .then(data => data.json())
+    .then(projects => projects.forEach(project => renderProject(project)));
+
+function renderProject({ date, name, url, stack }) {
+    const item = document.createElement("LI")
+    const project = document.createElement("A");
+    project.textContent = `${name} -||- ${date} -||- ${stack.join(", ")}`;
+    project.href = url;
+
+    item.appendChild(project);
+    list.appendChild(item);
+}
