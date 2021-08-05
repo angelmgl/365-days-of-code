06920afd03c7934c19dc26e544fca164c7e72bb2
Angel Mgl <angelemegeele@gmail.com>
2021-08-04 23:19:38 -0400

    feat: New design on the landing page

--- script.js
+++ script.js
@@ -1,15 +1,43 @@
-const list = document.querySelector("#list");
+const container = document.getElementById("projects");
 
 fetch("data.json")
     .then(data => data.json())
     .then(projects => projects.forEach(project => renderProject(project)));
 
 function renderProject({ date, name, url, stack }) {
-    const item = document.createElement("LI")
-    const project = document.createElement("A");
-    project.textContent = name;
-    project.href = url;
+    // create a new project row
+    let row = document.createElement("A");
+    row.setAttribute("href", url);
+    row.classList.add("row");
+    container.appendChild(row);
 
-    item.appendChild(project);
-    list.appendChild(item);
+    // create the title column
+    let titleColumn = document.createElement("DIV");
+    titleColumn.classList.add("col");
+    let title = document.createElement("SPAN");
+    title.textContent = name;
+    title.classList.add("title");
+    titleColumn.appendChild(title);
+    row.appendChild(titleColumn);
+
+    // create the date column
+    let dateColumn = document.createElement("DIV");
+    dateColumn.classList.add("col");
+    let dateSpan = document.createElement("SPAN");
+    dateSpan.textContent = date;
+    dateSpan.classList.add("date");
+    dateColumn.appendChild(dateSpan);
+    row.appendChild(dateColumn);
+
+    // create the stack column
+    let stackColumn = document.createElement("DIV");
+    stackColumn.classList.add("col");
+    stack.forEach(item => {
+        let img = document.createElement("IMG");
+        img.classList.add("tech");
+        img.setAttribute("src", `img/${item}.png`);
+        img.setAttribute("alt", item);
+        stackColumn.appendChild(img);
+    });
+    row.appendChild(stackColumn);
 }
